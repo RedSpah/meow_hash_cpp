b91ff8e66a9875f7b0929f9c1fdbf5499c676a45
redspah <redspah@gmail.com>
2018-10-22 00:45:40 +0100

    updated licensing

--- meowhash_cpp/meow_hash.hpp
+++ meowhash_cpp/meow_hash.hpp
@@ -23,10 +23,13 @@ See https://mollyrocket.com/meowhash for details.
 
 ========================================================================
 
-LICENSE
+
+zlib License
+
+(C) Copyright 2018 Molly Rocket, Inc.
 
 This software is provided 'as-is', without any express or implied
-warranty. In no event will the authors be held liable for any damages
+warranty.  In no event will the authors be held liable for any damages
 arising from the use of this software.
 
 Permission is granted to anyone to use this software for any purpose,
@@ -35,12 +38,10 @@ freely, subject to the following restrictions:
 
 1. The origin of this software must not be misrepresented; you must not
 claim that you wrote the original software. If you use this software
-in a product, an acknowledgement in the product documentation would be
+in a product, an acknowledgment in the product documentation would be
 appreciated but is not required.
-
 2. Altered source versions must be plainly marked as such, and must not be
 misrepresented as being the original software.
-
 3. This notice may not be removed or altered from any source distribution.
 
 ========================================================================
